8b44599def0b49545fa405212b1873ecb003007b
Cojol0511 <phunghoangphuloc@gmail.com>
2023-04-04 18:37:20 +0700

    update firebase and fix img

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,19 +1,19 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyA0Qxo2OwIp0Cf5PG64SHiIsBQXBpImb3Q",
-	authDomain: "sonquynhwed.firebaseapp.com",
+	apiKey: "AIzaSyDv0bn0PwqpwC-ZaK7mfsTpKOrHP_TXHLQ",
+	authDomain: "wedding-4062c.firebaseapp.com",
 	databaseURL:
-		"https://sonquynhwed-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "sonquynhwed",
-	storageBucket: "sonquynhwed.appspot.com",
-	messagingSenderId: "818832039726",
-	appId: "1:818832039726:web:fe6bf16a65cd72e47764bf",
+		"https://wedding-4062c-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "wedding-4062c",
+	storageBucket: "wedding-4062c.appspot.com",
+	messagingSenderId: "182269847951",
+	appId: "1:182269847951:web:b4a102d7d168d08aa303fa",
 };
 
 // initialize firebase
 firebase.initializeApp(firebaseConfig);
 
 // reference your database
-var contactFormDB = firebase.database().ref("SonQuynhWed");
+var contactFormDB = firebase.database().ref("WEDDING");
 
 document.getElementById("contactForm").addEventListener("submit", submitForm);
 
